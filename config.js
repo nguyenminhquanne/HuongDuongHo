ef53a3ec342165c077e69982ba816d20b51db6f4
nguyenminhquanne <104640893+nguyenminhquanne@users.noreply.github.com>
2022-04-29 22:25:44 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,17 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+    titleWeb: "Thế giới Cá Diên Chiên",
+    introTitle: 'Cô Gái Bán Cá Diên Chiên à',
+    introDesc: `Trái đất vốn lạ thường, mà em làm sao để anh vẫn nhớ hộp đậu bắp kèm theo hộp cá diên chiên :)))
+    btnIntro: 'Bí Mật Nằm Ở Đây',
+    title: 'Phải chăng em đăng muốn biết anh làm việc gì🥰',
+    desc: 'Phải chăng em đã nghĩ anh là 1 IT đúng không? ',
+    btnYes: 'Hônggggg anh à :3',
+    btnNo: 'Đúng, Em nghĩ Anh là 1 IT <3',
+    question: 'Bé Hưn Đườq Hổ muốn nói gì anh nữa hônggg <3',
+    btnReply: 'Gửiiiii nhaaaaaaa <3',
+    reply: 'Anh biết, em muốn gửi gì mà bấm gửi cho anh nha <33333333',
+    mess: 'Nói vậy thôi chứ anh cũng không biết em ghi gì đâu 🥰 haha 😘😘',
+    messDesc: 'Vậy em đã biết anh làm gì chưa :)))).',
+    btnAccept: 'Biết chớt liềnnnnn <3',
+    messLink: 'https://www.facebook.com/messages/t/nguyenminhquan300995' 
 }
